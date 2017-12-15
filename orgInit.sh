bf140652a8688cebebdae2a484c533443e70250a
Daan van Santen <daansan@gmail.com>
2017-12-15 21:11:45 +0100

    Third Commit

--- orgInit.sh
+++ orgInit.sh
@@ -2,6 +2,7 @@ sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
 sfdx force:source:push
 sfdx force:user:permset:assign -n Cat_Extra_Permission
 sfdx force:user:password:generate
+sfdx msm:data:file:upload -f ./assets/einstein_platform.pem
 sfdx force:apex:execute -f ./scripts/setupEinstein.cls
 sfdx force:data:tree:import -f ./data/Cat__cn.json
 sfdx force:data:tree:import -f ./data/Interested_Person__c.json
